d123809c84f43735d22aec12700e16e15273a978
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-10-10 16:44:14 -0700

    Encapsulate main task into def crawl

--- lake/__main__.py
+++ lake/__main__.py
@@ -1,28 +1,32 @@
 import asyncio
+import psycopg
 import suitcase
 
 from pathlib              import Path
 from playwright.async_api import async_playwright
 
-async def main() -> int:
-        """suitcase.main
+async def crawl() -> int:
+        """__main__.crawl
         """
-        
+
         async with async_playwright() as p:
                 browser = await p.chromium.launch(headless=False)
                 page    = await browser.new_page()
                 
                 await page.goto("https://www.ospo.noaa.gov/products/land/hms.html#data", wait_until="domcontentloaded")
-
-                handover = input("__main__.py, handover, [y/N]% ") == "y"
                 
-                if(not handover):
+                if(not input("") == ""):
                         await browser.close()
                         
                         return -1
  
                 await suitcase.session.download(await page.locator("a").evaluate_all(suitcase.scripts.read("injection.js"), suitcase.config.load()["filters"]), Path("common"), verbose=True)
-                await browser.close()
+        
+        return 0
+
+async def main() -> int:
+        """__main__.main
+        """
         
         return 0
 
