f224240951877ef987facb688cd3db99a37e7c4e
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-10-11 08:24:55 -0700

    Rename crawl() to scrape(). Modify whitespace

--- lake/__main__.py
+++ lake/__main__.py
@@ -1,26 +1,34 @@
 import asyncio
-import psycopg
 import suitcase
 
 from pathlib              import Path
 from playwright.async_api import async_playwright
 
-async def crawl() -> int:
-        """__main__.crawl
+async def scrape(
+                url: str 
+        ) -> int:
+        """__main__.scrape
         """
 
         async with async_playwright() as p:
                 browser = await p.chromium.launch(headless=False)
                 page    = await browser.new_page()
                 
-                await page.goto("https://www.ospo.noaa.gov/products/land/hms.html#data", wait_until="domcontentloaded")
+                await page.goto(url, wait_until="domcontentloaded")
                 
                 if(not input("") == ""):
                         await browser.close()
                         
                         return -1
  
-                await suitcase.session.download(await page.locator("a").evaluate_all(suitcase.scripts.read("injection.js"), suitcase.config.load()["filters"]), Path("common"), verbose=True)
+                await suitcase.session.download(
+                        await page.locator("a").evaluate_all(
+                                suitcase.scripts.read("injection.js"), 
+                                suitcase.config.load()["filters"]
+                        ), 
+                        Path("common"), 
+                        verbose=True
+                )
         
         return 0
 
