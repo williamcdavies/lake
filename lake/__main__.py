68ebf0bc26f24d7f9cb1205012b0ec7fbf25a7c5
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-10-18 10:54:57 -0700

    Major changes. Extracts all files from common and inserts them into spatial

--- lake/__main__.py
+++ lake/__main__.py
@@ -1,8 +1,15 @@
 import asyncio
-import suitcase # type: ignore
+import suitcase
+import tempfile
+import zipfile
+
+import geopandas as gpd
 
 from pathlib              import Path
 from playwright.async_api import async_playwright
+from sqlalchemy           import create_engine
+
+_COMMON_PATH = Path(Path(__file__).parent.parent / "common")
 
 async def scrape(
                 url: str 
@@ -32,11 +39,53 @@ async def scrape(
         
         return 0
 
-async def main() -> int:
+def main() -> int:
         """__main__.main
         """
+
+        engine     = create_engine("postgresql://williamchuter-davies@localhost:5432/spatial")
+        table_dict = {
+                "hms_fire": "hms_fires",
+                "hms_smoke": "hms_smokes"
+        }
+
+        for path in _COMMON_PATH.glob("*.zip"):
+                try:
+                        with zipfile.ZipFile(path, 'r') as zf:
+                                shp_files = [f for f in zf.namelist() if f.endswith(".shp")]
+                        
+                                if not shp_files:
+                                        continue
+                                
+                                shp_file = shp_files[0]
+                                
+                                with tempfile.TemporaryDirectory(dir=_COMMON_PATH) as buffer:
+                                        zf.extractall(buffer)
+
+                                        stem  = Path(shp_file).stem.lower()
+                                        table = next((v for k, v in table_dict.items() if k in stem), None)
+
+                                        if not table:
+                                                continue
+                                        
+                                        try:
+                                                gdf = gpd.read_file(Path(buffer) / shp_file, on_invalid="fix")
+                                        
+                                        except Exception as e:
+                                                print(f"Insertion of {shp_file} from {buffer} to {table}: Failed with {e}")
+                                                
+                                                continue
+
+                                        if table:
+                                                print(f"Insertion of {shp_file} from {buffer} to {table}: Success")
+                                                gdf.to_postgis(table, engine, if_exists="append", index=False, chunksize=1024)
+               
+                except zipfile.BadZipFile as e:
+                        print(f"Open {path} as ZipFile: Failed with {e}")
+                        
+                        continue
         
-        return await scrape("https://www.ospo.noaa.gov/products/land/hms.html#data")
+        return 0
 
 if __name__ == '__main__':
-        asyncio.run(main())
+        main()
