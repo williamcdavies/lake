f4a77983237a6836c56131ecbc788f10d57637ab
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-10-18 14:16:30 -0700

    Comment out use-specific code blocks for clarity

--- lake/__main__.py
+++ lake/__main__.py
@@ -1,13 +1,14 @@
-# import asyncio
-# import suitcase
-# from   playwright.async_api import async_playwright
-
+import asyncio
 import re
+import suitcase
 import tempfile
 import zipfile
-import geopandas  as     gpd
-from   pathlib    import Path
-from   sqlalchemy import create_engine
+
+import geopandas as gpd
+
+from pathlib              import Path
+from playwright.async_api import async_playwright
+from sqlalchemy           import create_engine
 
 _COMMON_PATH = Path(Path(__file__).parent.parent / "common")
 
@@ -40,54 +41,63 @@ _COMMON_PATH = Path(Path(__file__).parent.parent / "common")
 #         return 0
 
 def main() -> int:
-    """__main__.main
-    """
+        """__main__.main
+        """
     
-    engine          = create_engine("postgresql://williamchuter-davies@localhost:5432/spatial")
-    table_dict = {
-        "hms_fire": "hms_fires",
-        "hms_smoke": "hms_smokes"
-    }
+        engine = create_engine("postgresql://williamchuter-davies@localhost:5432/spatial")
+        
 
-    for path in sorted(_COMMON_PATH.glob("*.zip")):
-        try:
-                with zipfile.ZipFile(path, 'r') as zf:
-                        shp_files = [f for f in zf.namelist() if f.endswith(".shp")]
-                
-                        if not shp_files:
-                                continue
+        # FOR HydroLAKES_[points | polys]_v10_shp ZipFile(s)
 
-                        shp_file = shp_files[0]
+        # gdf = gpd.read_file(Path(_COMMON_PATH / "HydroLAKES_polys_v10_shp/HydroLAKES_polys_v10_shp/HydroLAKES_polys_v10.shp"), on_invalid="fix")
+        # gdf.to_postgis("lakes_polys", engine, if_exists="append", index=False, chunksize=1024)
 
-                        with tempfile.TemporaryDirectory(dir=_COMMON_PATH) as buffer:
-                                zf.extractall(buffer)
+        # FOR HM_FIRE20XXMMDD ZipFile(s)
 
-                                stem = Path(shp_file).stem.lower()
-                                key  = next((k for k in table_dict if k in stem), None)
-                    
-                                if not key:
-                                        continue
+        # table_dict = {
+        #         "hms_fire": "hms_fires",
+        #         "hms_smoke": "hms_smokes"
+        # }
 
-                                year  = re.search(r"(\d{4})", stem)
-                                table = f"{table_dict[key]}{year.group(1) if year else "unknown"}"
+        # for path in sorted(_COMMON_PATH.glob("*.zip")):
+        #         try:
+        #                 with zipfile.ZipFile(path, 'r') as zf:
+        #                         shp_files = [f for f in zf.namelist() if f.endswith(".shp")]
+                        
+        #                         if not shp_files:
+        #                                 continue
 
-                                try:
-                                        gdf = gpd.read_file(Path(buffer) / shp_file, on_invalid="fix")
-                                
-                                except Exception as e:
-                                        print(f"Insertion of {shp_file} from {buffer} to {table}: Failed with {e}")
+        #                         shp_file = shp_files[0]
+
+        #                         with tempfile.TemporaryDirectory(dir=_COMMON_PATH) as buffer:
+        #                                 zf.extractall(buffer)
+
+        #                                 stem = Path(shp_file).stem.lower()
+        #                                 key  = next((k for k in table_dict if k in stem), None)
+                        
+        #                                 if not key:
+        #                                         continue
+
+        #                                 year  = re.search(r"(\d{4})", stem)
+        #                                 table = f"{table_dict[key]}{year.group(1) if year else "unknown"}"
+
+        #                                 try:
+        #                                         gdf = gpd.read_file(Path(buffer) / shp_file, on_invalid="fix")
                                         
-                                        continue
+        #                                 except Exception as e:
+        #                                         print(f"Insertion of {shp_file} from {buffer} to {table}: Failed with {e}")
+                                                
+        #                                         continue
 
-                                print(f"Insertion of {shp_file} from {buffer} to {table}: Success")
-                                gdf.to_postgis(table, engine, if_exists="append", index=False, chunksize=1024)
+        #                                 print(f"Insertion of {shp_file} from {buffer} to {table}: Success")
+        #                                 gdf.to_postgis(table, engine, if_exists="append", index=False, chunksize=1024)
 
-        except zipfile.BadZipFile as e:
-            print(f"Attempt to open {path} as ZipFile: Failed with {e}")
-            
-            continue
+        #         except zipfile.BadZipFile as e:
+        #         print(f"Attempt to open {path} as ZipFile: Failed with {e}")
+                
+        #         continue
 
-    return 0
+        return 0
 
 if __name__ == '__main__':
         main()
